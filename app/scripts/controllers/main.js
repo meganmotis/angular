d0434260d0306dde97057b374e82eb39eaeac071
Karina Wolff <karinaschmidt@hotmail.com>
2015-12-03 14:16:42 -0800

    added more photos, improved css

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -29,24 +29,36 @@ angular.module('angularAppApp')
     };
     $scope.showOutfit = function(temperature, humidity){
       if (humidity >= 73) {
-        $scope.myPicture="images/Eli.jpg";
+        $scope.myPicture="images/rainy.jpg";
         // show picture of rain contact
       }
       else {
         if (temperature > 25) {
-          console.log("veryhot");
+          // console.log("veryhot");
           // very hot summer picture
           $scope.myPicture = "images/veryhot.jpg";
         }
-        if (temperature >17 && temperature <=25) {
+        if (temperature >20 && temperature <=25) {
           // warm weather
           $scope.myPicture="images/Eli.jpg";
         }
-        if (temperature >6 && temperature <=17) {
+        if (temperature >15 && temperature <=20) {
           // chilly
           $scope.myPicture="images/chilly.jpg";
         }
-        if (temperature <=6) {
+        if (temperature >10 && temperature <=15) {
+          // chilly
+          $scope.myPicture="images/cool.jpg";
+        }
+        if (temperature >5 && temperature <=10) {
+          // chilly
+          $scope.myPicture="images/fall_leaves.jpg";
+        }
+        if (temperature >0 && temperature <=5) {
+          // chilly
+          $scope.myPicture="images/cold.jpg";
+        }
+        if (temperature <=0) {
           // very cold
           $scope.myPicture="images/verycold.jpg";
         }
